b33c1b1a9142d1c5690202df1df339c703b1d16f
Tim Brown <twbrown@andrew.cmu.edu>
2015-03-04 20:02:18 -0500

    added catkinized project

--- high-level/rosbuild/developers.py
+++
@@ -1,7 +0,0 @@
-{developers:
-    "twbrown"   : "Tim Brown",
-    "zherengz"  : "Zhereng Zhang",
-    "ehcai"     : "Edward Cai",
-    "skrulcik"  : "Scott Krulcik",
-    "jaehob"    : "Jaeho Bang"
-}
--- high-level/rosbuild/scout_avr/src/orb.h
+++
@@ -1,8 +0,0 @@
-#ifndef ORB_H
-#define ORB_H
-
-void orb_init();
-void orb_set0(unsigned char r, unsigned char g, unsigned char b);
-void orb_set1(unsigned char r, unsigned char g, unsigned char b);
-
-#endif
